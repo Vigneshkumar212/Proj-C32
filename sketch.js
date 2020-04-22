470e2b3bf64eebe0cc9a70fac5e8f5f558cfe045
Vigneshkumar <60309361+Vigneshkumar212@users.noreply.github.com>
2020-04-22 22:19:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,60 +3,92 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+var pri,vin,vig,bbird = "sprites/vinod.png";
+var bpig = "sprites/vinod.png";
+
 var engine, world;
-var box1, pig1,pig3;
+var box1, pig1,pig3,pimg,vimg,viimg;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var GameState = "select";
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var playerimg,pigimg,ky = "vig" ;
 var score = 0;
 
-var gameState = "onSling";
-var bg = "sprites/school.jpg";
-
 function preload() {
-
-     GetTime();
-
+    GetTime();
+    
 }
 
 function setup(){
+
+    
+
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
     
-
+     
     ground = new Ground(600,height,1200,20);
-    
-    box1 = new Box1(700,320,70,70);
-    box2 = new Box1(920,320,70,70);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+    pig4 = new Pig(1050, 150);
     log1 = new Log(810,260,300, PI/2);
-    
-    box3 = new Box1(700,250,70,70);
-    box4 = new Box1(920,250,70,70);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+
+    pig5 = new Pig(600, 380);
+    pig6 = new Pig(770, 180);
+    pig7 = new Pig(840, 180);
+
+
     log3 =  new Log(810,180,300, PI/2);
 
-    box5 = new Box(810,170,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+   
+    box6 = new Box(1050,380,100,200);
+
+
 
     bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:195, y:220});
+    pir = new CreateSprite(100,150,1,1);
+    vig = new CreateSprite(400,150,1,1);
+    vin = new CreateSprite(800,150,1,1);
+
+     pimg = loadImage("sprites/pe.png");
+     viimg = loadImage("sprites/vinod.png");
+     vimg = loadImage("sprites/vk.png");
+    
 }
 
 function draw(){
-    background(225);
-    if(backgroundImg){
-    background(backgroundImg);
-    }
+    background("white");
+
+
+
+    if(backgroundImg)
+        background(backgroundImg);
+    
+
+    
     Engine.update(engine);
-    textSize(50);
-    strokeWeight(4);
-    text("Your Score : " + score, 30, 50);
+    //strokeWeight(4);
+
+    if(GameState === "play" ){
+ 
+        noStroke();
+        textSize(50);
+        strokeWeight(4);
+        text("Your Score : " + score, 30, 50);
 
     box1.display();
     box1.score();
@@ -74,48 +106,123 @@ function draw(){
     box4.score();
     pig3.display();
     pig3.score();
+
+    pig5.display();
+    pig5.score();
+
+    pig6.display();
+    pig6.score();
+
+    pig7.display();
+    pig7.score();
+
+    pig4.display();
+    pig4.score();
     log3.display();
     log3.score();
 
-    box5.display();
-    box5.score();
-    log4.display();
-    log4.score();
-    log5.display();
-    log5.score();
+  
+    box6.display();
+    box6.score();
 
-    bird.display();
-    slingshot.display();   
-    textSize(15);
-    text("weak point is here (hit hard) --->>>",430,360)
+
+
     
+    //log6.display();
+    slingshot.display();   
+    bird.display();
+   
+     
+    image(playerimg, bird.body.position.x-50, bird.body.position.y-50, 125, 125);
+    
+   }
+
+    if(GameState === "select"){
+
+        
+        
+        strokeWeight(4);
+        fill("yellow");
+        text("Select The Player You Want",350,100)
+        textSize(20);
+        text("For player 1 press 1 on keyboard",70,380);
+        text("For player 2 press 2 on keyboard",430,380);
+        text("For player 3 press 3 on keyboard",800,380);
+        fill("white");
+        textSize(50);
+        text("1", pir.body.position.x , pir.body.position.y)
+        text("2", vig.body.position.x + 60, vig.body.position.y)
+        text("3", vin.body.position.x + 60, vin.body.position.y)
+        pir.display();
+        
+        image(pimg, pir.body.position.x - 50, pir.body.position.y -50, 250, 250);
+
+        vig.display();
+        
+        image(vimg, vig.body.position.x -20, vig.body.position.y-100, 350, 350);
+        vin.display();
+        
+        image(viimg, vin.body.position.x-50, vin.body.position.y-150, 450, 450);
+
+        if(keyCode === 49){
+            bpig = "sprites/vk.png";
+            bbird = "sprites/pe.png";
+            GameState = "play";
+            pir.body.position.y = -2000;
+            vig.body.position.y = -2000;
+            vin.body.position.y = -2000;
+          }
+          if(keyCode === 50){
+            bpig = "sprites/vinod.png";
+              bbird = "sprites/vk.png";
+              GameState = "play";
+              pir.body.position.y = -2000;
+              vig.body.position.y = -2000;
+              vin.body.position.y = -2000;
+          }
+          if(keyCode === 51){
+            bpig = "sprites/pe.png";
+              bbird = "sprites/vinod.png";
+              GameState = "play";
+              pir.body.position.y = -2000;
+              vig.body.position.y = -2000;
+              vin.body.position.y = -2000;
+          }
+          pigimg = loadImage (bpig);
+          playerimg = loadImage (bbird);
+
+
+          
+    }
+     
 }
 
 function mouseDragged(){
-    if (gameState!=="launched"){
+    if (GameState ==="play" /*&&  gameState!=="launched"*/){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
     }
 }
 
 
 function mouseReleased(){
-     
     slingshot.fly();
     gameState = "launched";
+
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-        Matter.Body.setPosition(ball.body, {x: 100 , y: 100});
-        slingshot.attach(bird.body);
+    if(keyCode === 32 && GameState === "play"){
+        gameState = "hehehe";
+
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body,{x: 220 , y:50})
+       slingshot.attach(bird.body);
     }
 }
 
-
-
 async function GetTime(){
 
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
   
     var responsejson = await response.json();
 
@@ -134,4 +241,5 @@ async function GetTime(){
     backgroundImg  = loadImage (bg);
 
     console.log(hour);
+
 }
